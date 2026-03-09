98abc9d4ff7093da6ae52dd24f403180ee6bf079
Paulo Brito <pricardof27@gmail.com>
2026-03-09 20:39:38 -0300

    feat: configuração inicial do projeto react
    - Criação do projeto com npm create vite
    - Remoção de arquivos desnecessários
    - Criação de componentes React básicos
    - Estruturação inicial da página com inputs, buttons e labels

---
+++ ap03-react-components-jsx/src/App.jsx
@@ -0,0 +1,17 @@
+/************************************************* 
+ *                                               *  
+ *          MEU PRIMEIRO COMPONENTE REACT        *
+ *                                               * *************************************************/
+//AP03
+//JSX - Java Script Extension
+
+//Expressão JSX - Função JS que devolve uma DIV 
+const App = () => {
+    return <div>
+        Meu primeiro componente React
+    </div>
+}
+
+export default App
+
+
